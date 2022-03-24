54e5ffcc1bd5e638491aca17b0a15f14fdd41391
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-03-24 19:35:04 -0300

    Se agrega el proyecto

---
+++ src/models/Main2.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// variable entera
+		//forma larga
+		int edad;
+		edad = 23;
+		System.out.println("Mi edad es: "+edad);
+		
+		
+		// variable de tipo String
+		// forma corta
+		String saludo ="Hola chicos!";
+		System.out.println(saludo);
+		
+		// variable real
+		double precioDelPan;
+		precioDelPan = 37.13;
+		System.out.println("El precio del pan es: $"+ precioDelPan);
+		precioDelPan = 50;
+		System.out.println("El pan aumento ha: $"+ precioDelPan);
+		
+		// variable de tipo char
+		char caracter;
+		caracter = '#';
+		caracter = '1';
+		System.out.println("El caracter es: "+ caracter);
+		
+		// variable boolean
+		boolean esMujer = true;
+		System.out.println("Es mujer?: "+ esMujer);
+	}
+
+}
